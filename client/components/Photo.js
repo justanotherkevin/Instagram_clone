4ccd686c88ea01ecb01fee957210edf7cfaeab7a
kevin hu <hu.kevin.w@gmail.com>
2017-07-30 11:24:20 -0400

    Add individual photo componemnt

---
+++ client/components/Photo.js
@@ -0,0 +1,13 @@
+import React from 'React';
+
+const Photo = React.createClass({
+    render() {
+        return (
+            <figure clasName="grid-figure">
+                I am a photo!
+            </figure>
+        )
+    }
+})
+
+export default Photo;
