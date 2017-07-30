a808143f2353b51d51343b1cba4663d9d2b77271
kevin hu <hu.kevin.w@gmail.com>
2017-07-30 15:24:29 -0400

    Add case statement to increment likes

--- client/reducers/posts.js
+++ client/reducers/posts.js
@@ -1,5 +1,15 @@
+// make a priditable and pure function: take the action, make a copy, return the copy
 function posts(state = [], action) {
-    console.log(state, action );
-    return state;
+    switch(action.type) {
+        case 'INCREMENT_LIKES' :
+            const index = action.index;
+            return [
+                ...state.slice(0,index), // before the one we are updating
+                {...state[index], likes: state[index].likes + 1},
+                ...state.slice(index + 1), // after the one we are updating
+            ]
+        default:
+            return state;
+    }
 }
-export default posts; 
+export default posts;
