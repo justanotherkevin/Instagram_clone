eb900298b54e50b25099a712f4c7223de5739f1a
kevin hu <hu.kevin.w@gmail.com>
2017-07-29 23:25:42 -0400

    Add action dispacher

---
+++ client/actions/actionCreator.js
@@ -0,0 +1,24 @@
+// increase #likes
+export function increment(index) {
+    return {
+        type: 'INCREMENT_LIKES',
+        index
+    }
+}
+// add comments
+export function addComment(postId, author, comment) {
+    return {
+        type: 'ADD_COMMENT',
+        postId,
+        author,
+        comment
+    }
+}
+
+export function addComment(postId, i) {
+    return {
+        type: 'REMOVE_COMMENT',
+        postId,
+        i
+    }
+}
