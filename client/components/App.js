a617c0cb9c3f979ddb9cbfbf12a92cb82647a8ce
kevin hu <hu.kevin.w@gmail.com>
2017-07-30 11:24:58 -0400

    Add App to connect component with redux

---
+++ client/components/App.js
@@ -0,0 +1,19 @@
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import * as actionCreators from '../actions/actionCreators';
+import Main from './Main';
+//
+function mapStateToProps(state) {
+  return {
+    posts: state.posts,
+    comments: state.comments
+  }
+}
+
+function mapDispachToProps(dispatch) {
+  return bindActionCreators(actionCreators, dispatch);
+}
+// the function connect will pass props into the commponent (in this case Main)
+const App = connect(mapStateToProps, mapDispachToProps)(Main);
+
+export default App;
