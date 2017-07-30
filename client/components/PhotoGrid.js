29e130438b2d79c49defcc31237cd6f9f9449d24
kevin hu <hu.kevin.w@gmail.com>
2017-07-30 11:26:25 -0400

    Import photo component to pgotofird

--- client/components/PhotoGrid.js
+++ client/components/PhotoGrid.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Photo from './Photo';
 
  const PhotoGrid = React.createClass({
     render() {
         return (
             <div className="photo-grid">
-                I am photogrid
+
             </div>
         )
     }
