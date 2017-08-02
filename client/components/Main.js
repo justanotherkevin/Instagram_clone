196e2a23e7101d1d3b72c3e76d8d45b1fc03ae55
kevin hu <hu.kevin.w@gmail.com>
2017-08-02 12:37:21 -0400

    Adding div-tag for each letter

--- client/components/Main.js
+++ client/components/Main.js
@@ -5,9 +5,22 @@ import { Link } from 'react-router';
     render() {
         return (
             <div>
-                <h1>
-                    <Link to="/">Reduxstagram </Link>
-                </h1>
+                <Link className="a-tag" to="/">
+                    <h1 className="title-red">
+                        <div className="rr animated bounceInDown upcase">r</div>
+                        <div className="re animated bounceInDown">e</div>
+                        <div className="rd animated bounceInDown">d</div>
+                        <div className="ru animated bounceInDown">u</div>
+                        <div className="rx animated bounceInDown">x</div>
+                        <div className="rs animated bounceInDown">s</div>
+                        <div className="rt animated bounceInDown">t</div>
+                        <div className="ra animated bounceInDown">a</div>
+                        <div className="rg animated bounceInDown">g</div>
+                        <div className="rr animated bounceInDown">r</div>
+                        <div className="ra animated bounceInDown">a</div>
+                        <div className="rm animated bounceInDown">m</div>
+                    </h1>
+                </Link>
                 {React.cloneElement(this.props.children, this.props)}
             </div>
         )
