b0c22f1a7d056bbfe299dac3aa1683cd7026aff2
kevin hu <hu.kevin.w@gmail.com>
2017-08-30 19:24:37 -0400

    Dotenv package dependency

--- client/data/posts.js
+++ client/data/posts.js
@@ -1,52 +1,59 @@
+import Unsplash from 'unsplash-js';
+
+const unsplash = new Unsplash({
+  applicationId: "{APP_ID}",
+  secret: "{APP_SECRET}",
+  callbackUrl: "{CALLBACK_URL}"
+});
 const posts = [
    {
       "code":"BAcyDyQwcXX",
       "caption":"Lunch #hamont",
       "likes":56,
       "id":"1161022966406956503",
-      "display_src":"https://scontent.cdninstagram.com/hphotos-xap1/t51.2885-15/e35/12552326_495932673919321_1443393332_n.jpg"
+      "display_src":"https://unsplash.com/?photo=_l8ZdgJ9m7w"
    },
    {
       "code":"BAcJeJrQca9",
       "caption":"Snow! ⛄️🌨❄️ #lifewithsnickers",
       "likes":59,
       "id":"1160844458347054781",
-      "display_src":"https://scontent.cdninstagram.com/hphotos-xaf1/t51.2885-15/e35/12407344_1283694208323785_735653395_n.jpg"
+      "display_src":"https://unsplash.com/?photo=usk52YRwGrM"
    },
    {
       "code":"BAF_KY4wcRY",
       "caption":"Cleaned my office and mounted my recording gear overhead. Stoked for 2016!",
       "likes":79,
       "id":"1154606670337393752",
-      "display_src":"https://scontent.cdninstagram.com/hphotos-xpf1/t51.2885-15/e35/923995_1704188643150533_1383710275_n.jpg"
+      "display_src":"https://unsplash.com/?photo=tLZC43tzhTo"
    },
    {
       "code":"BAPIPRjQce9",
       "caption":"Making baby pancakes for one early rising baby. ☕️🍴",
       "likes":47,
       "id":"1157179863266871229",
-      "display_src":"https://scontent.cdninstagram.com/hphotos-xap1/t51.2885-15/e35/12407480_1654828594805097_152207166_n.jpg"
+      "display_src":"https://unsplash.com/?photo=DoveRW4-cMo"
    },
    {
       "code":"-hZh6IQcfN",
       "caption":"New Stickers just came in. I'll do another mailing in a few weeks if you want some. #javascript",
       "likes":66,
       "id":"1126293663140399053",
-      "display_src":"https://scontent.cdninstagram.com/hphotos-xap1/t51.2885-15/e35/11875511_1562439187344831_813588280_n.jpg"
+      "display_src":"https://unsplash.com/?photo=8ELx7tiDxY0"
    },
    {
       "code":"-B3eiIwcYV",
       "caption":"Tacos for breakfast. I love you Austin. 🇺🇸",
       "likes":33,
       "id":"1117418173361145365",
-      "display_src":"https://scontent.cdninstagram.com/hphotos-xfa1/t51.2885-15/e35/11917950_927755223968499_1198055371_n.jpg"
+      "display_src":"https://unsplash.com/?photo=6VjPmyMj5KM"
    },
    {
       "code":"BAhvZrRwcfu",
       "caption":"Tried poke for the first time at @pokehbar. Delicious! It's like a bowl of sushi",
       "likes":30,
       "id":"1162418651480049646",
-      "display_src":"https://scontent.cdninstagram.com/hphotos-xpa1/t51.2885-15/e35/12501993_1504179163220771_2060674913_n.jpg"
+      "display_src":"https://unsplash.com/?photo=MctEgCk1Dm0"
    },
    {
       "code":"BAAJqbOQcW5",
